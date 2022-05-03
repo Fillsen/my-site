f4caf8a24d58be80a30171ba25b11b34ec454e3e
m.golubtsov <51379917+Fillsen@users.noreply.github.com>
2022-05-03 13:49:42 +0300

    continue init default structure

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default {
+  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+  ssr: true,
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
@@ -34,11 +38,43 @@ export default {
   buildModules: [
   ],
 
+  styleResources: {
+    /* Do not import actual styles. Use this module only to import variables,
+		mixins, functions (et cetera) as they won't exist in the actual build.
+		Importing actual styles will include them in every component and
+		will also make your build/HMR magnitudes slower. Do not do this!
+     */
+    scss: [
+      '~/assets/styles/vars/_colors.scss',
+      '~/assets/styles/vars/_fonts.scss',
+      '~/assets/styles/vars/_screens.scss',
+      '~/assets/styles/vars/_text.scss',
+    ],
+  },
+
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios',
   ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {
+    baseURL:
+      process.env.BASE_URL ||
+      '',
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+    publicPath: process.env.PUBLIC_PATH,
+    filenames: {
+      app: ({ isDev }) => (isDev ? '[name].js' : '[name].js?h=[contenthash]'),
+      chunk: ({ isDev }) => (isDev ? '[name].js' : '[name].js?h=[contenthash]'),
+      font: '[name].[ext]',
+    },
+  },
+
+  env: {
+    baseURL: process.env.BASE_URL || 'http://localhost:3000'
+  },
 }
