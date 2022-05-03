1f982c0000481d16a94ab9fd707a715a67647a6d
m.golubtsov <51379917+Fillsen@users.noreply.github.com>
2022-05-03 16:00:00 +0300

    layout system init

---
+++ components/layout/TheFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+FOOTER
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="layout-default">
+    <TheHeader />
+    <main
+      id="main"
+      role="main"
+    >
+      <Nuxt />
+    </main>
+    <TheFooter />
+  </div>
+</template>
+
+
+<script>
+import TheHeader from "~/components/layout/TheHeader";
+import TheFooter from "~/components/layout/TheFooter";
+
+export default {
+  name: 'DefaultLayout',
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
